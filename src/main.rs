7c471f901fec7402ab8dad9b4f70fc9a2e0fe6a7
Pjiwm <pim.munne@gmail.com>
2022-04-18 00:50:38 +0200

    entites move to left

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@ use specs_derive::Component;
 use std::cmp::{max, min};
 
 #[derive(Component)]
+struct LeftMover {}
+
+impl<'a> System<'a> for LeftMover {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79};
+        }
+    }
+}
+
+#[derive(Component)]
 struct Position {
     x: i32,
     y: i32,
@@ -20,9 +34,18 @@ struct State {
     ecs: World,
 }
 
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftMover{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+        self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
@@ -33,12 +56,11 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
-        .build()?;
+    let context = RltkBuilder::simple80x50().with_title("Web Game").build()?;
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
     for i in 0..10 {
         gs.ecs
             .create_entity()
@@ -48,6 +70,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::YELLOW),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
 
